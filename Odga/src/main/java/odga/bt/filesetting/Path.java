b3d6ce06ea2b1771a9a68cee259e7bd4cb888ffd
DobbyisFree1 <doby14515@gmail.com>
2021-03-18 12:27:50 +0900

    ODGAPROJECT v1.4
    Good boy~

---
+++ Odga/src/main/java/odga/bt/filesetting/Path.java
@@ -0,0 +1,8 @@
+package odga.bt.filesetting;
+
+public class Path {
+	public static final String FILE_STORE = "C:/coding/spring/sts-bundle/sts-3.9.11.RELEASE/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Odga/resources/assets/img/profile/";
+	public static final String FILE_REVIEW = "C:/coding/spring/sts-bundle/sts-3.9.11.RELEASE/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Odga/resources/upload/";
+
+}
+
